8f0f2262ebc533edb472047ca91449164ce80a6e
Michelle Phung <michellephung@gmail.com>
2014-12-05 16:17:05 -0500

    changed Fauxton username variable in testing

--- test/nightwatch_tests/custom-commands/loginToGUI.js
+++ test/nightwatch_tests/custom-commands/loginToGUI.js
@@ -7,13 +7,13 @@ exports.command = function () {
 
   client
     .url(baseUrl+'/#login')
-    .waitForElementPresent('a[href="#login"]', 8000, false)
+    .waitForElementPresent('a[href="#login"]', 15000, false)
     .click('a[href="#login"]')
-    .waitForElementPresent('#username', 8000, false)
+    .waitForElementPresent('#username', 10000, false)
     .setValue('#username', [username])
     .setValue('#password', [password, client.Keys.ENTER])
     .closeNotification()
-    .waitForElementPresent('#jump-to-db', 8000, false);
+    .waitForElementPresent('#jump-to-db', 10000, false);
 
   return this;
 };
