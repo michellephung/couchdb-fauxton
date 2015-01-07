d35dad683a6f86399ada6a89fe9eff856e5befc3
michellephung@gmail.com <michellephung@gmail.com>
2015-01-07 15:44:56 -0500

    reset permissions url to old url

--- app/addons/documents/base.js
+++ app/addons/documents/base.js
@@ -105,20 +105,5 @@ function(app, FauxtonAPI, Documents) {
     }
   });
 
-  //will put this into permissions folder later
-  FauxtonAPI.registerUrls( 'permissions', {
-    server: function (id) {
-      return app.host + '/database/' + id  + '/permissions';
-    },
-
-    app: function (id) {
-      return 'database/' + id;
-    },
-
-    apiurl: function (id) {
-      return window.location.origin + '/_api/v2/db/'+ id + '/_security' ;
-    }
-  });
-
   return Documents;
 });
